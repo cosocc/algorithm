85619451015c4f82516bf2b5a025c94425bfdb22
光湖 杨 <121907007@qq.com>
2022-01-15 03:03:38 +0800

    test first 2添加项目文件。

---
+++ strlen/strlen.cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+size_t strlenx(const char* str) {
+	const char* s;
+	for (s = str; *s; ++s) {}
+	return(s - str);
+}
+int main(int argc, char** argv)
+{
+	const char* str = "hellojds";
+
+	size_t a = strlenx(str);
+	printf("%d", a);
+
+	return 0;
+}
+
